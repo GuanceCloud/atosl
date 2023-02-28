b7483c3cea6ac70352f36ab5a8a16e70a38b7f43
zhangyi <zhangyi905@guance.com>
2023-02-28 13:11:21 +0800

    add libz to the linker，set CC=clang for macOS and gcc for Linux add libdwarf default installation prefix to gcc -Wl,-rpath upgrade version to v1.1.1

--- config.mk
+++ config.mk
@@ -1,10 +1,16 @@
-VERSION = 1.1
+VERSION = 1.1.1
 
 PREFIX = /usr/local
 
 CFLAGS =-I /usr/include/libdwarf -Wall -Werror -O2 -DATOSL_VERSION=\"${VERSION}\"
-LDFLAGS = -ldwarf -liberty
+LDFLAGS = -ldwarf -liberty -lz
 
+OS = $(shell uname -s)
+ifeq ($(OS), Darwin)
 CC = clang
+else
+CC = gcc
+LDFLAGS := $(LDFLAGS) -Wl,-rpath=/usr/local/lib:/usr/local/lib64
+endif
 
 -include config.mk.local
